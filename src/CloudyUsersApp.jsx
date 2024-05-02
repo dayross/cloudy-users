31493ad86f21958cbd7a63d4c9f99db2daa7e888
Diana Zapata <dianarocio.czapata@gmail.com>
2024-05-02 14:29:16 -0600

    main app. ocupo calarla

--- src/CloudyUsersApp.jsx
+++ src/CloudyUsersApp.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { getUsers } from './helpers/getUsers';
+import {showUser} from './components/showUser';
 
-export const CloudyUsersApp = () => {
-  return (
-    <div>CloudyUsersApp</div>
+export const CloudyUsersApp = async() => {
+    // guardamos los usuarios en un arreglo
+    const [users, setUsers] = useState([]);
+    // mandamos llamar el fetch de usuarios
+    const newUsers = await getUsers();
+    // los agregamos al arreglo
+    setUsers(newUsers);
+
+    return (
+        <>
+            <h3> Los usuarios de nuestra base de datos son...</h3>
+
+            {users.map( (user) => (
+                <showUser key={user.id} {...user} />
+            ))}
+        </>
+        
   )
 }
