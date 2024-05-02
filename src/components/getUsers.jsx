3da67b401f2a8417f98a8caa4a0ed97a8cae0db6
Diana Zapata <dianarocio.czapata@gmail.com>
2024-05-02 14:14:06 -0600

    users fetch

--- src/components/getUsers.jsx
+++ src/components/getUsers.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
 
-export const getUsers = () => {
-  
+export const getUsers = async() => {
+  const url = `http://129.158.44.95:3000/users`;
+  const resp = await fetch(url);
+  const {data} = resp.json();
+  const users = data.map(user => ({
+    id : user.id,
+    name : user.name,
+    email : user.email
+}));
+return users;
 }
-
-
